e6810e0a0559a59dd75be85115a525bf0cb31839
wadhamst <wadhamst@users.noreply.github.com>
2018-04-27 09:38:08 -0400

    Initial Commit

---
+++ cube.js
@@ -0,0 +1,10 @@
+$(".side").on("click", "li", function(){
+  $("#cube").removeClass().addClass($(this).attr("class"));
+}).on("mouseover", "li", function(){
+  var me   = $(this);
+  var attr = me.attr("data-url");
+
+  if(attr != undefined){
+    $("h2").text(attr).css("visibility", "visible");
+  }
+});
